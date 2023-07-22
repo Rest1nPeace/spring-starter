f6129004c5571b04676977c9d4c33678e09591ec
Rest1nPeace <vitaliti081@gmail.com>
2023-07-22 15:40:04 +0300

    Initial commit

---
+++ src/main/java/com/dmdev/spring/database/repository/CompanyRepository.java
@@ -0,0 +1,45 @@
+package com.dmdev.spring.database.repository;
+
+import com.dmdev.spring.bpp.Auditing;
+import com.dmdev.spring.bpp.Transaction;
+import com.dmdev.spring.database.entity.Company;
+import com.dmdev.spring.database.pool.ConnectionPool;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+@Scope(BeanDefinition.SCOPE_PROTOTYPE)
+@Transaction
+@Auditing
+@RequiredArgsConstructor
+public class CompanyRepository implements CrudRepository<Integer, Company> {
+
+    private final ConnectionPool pool1;
+    private final List<ConnectionPool> pools;
+    @Value("${db.pool.size}")
+    private final Integer poolSize;
+
+    @PostConstruct
+    private void init() {
+        System.out.println("init company repository");
+    }
+
+    @Override
+    public Optional<Company> findById(Integer id) {
+        System.out.println("findById method...");
+        return Optional.of(new Company(id));
+    }
+
+    @Override
+    public void delete(Company entity) {
+        System.out.println("delete method...");
+    }
+}
