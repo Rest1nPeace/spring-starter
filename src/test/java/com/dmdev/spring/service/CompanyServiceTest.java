038f5c0ee86e9655e63eb5abc41ac61e8c78a20e
Rest1nPeace <vitaliti081@gmail.com>
2023-07-23 21:49:44 +0300

    57.-_Query

--- src/test/java/com/dmdev/spring/service/CompanyServiceTest.java
+++ src/test/java/com/dmdev/spring/service/CompanyServiceTest.java
@@ -1,22 +1,23 @@
 package com.dmdev.spring.service;
 
 import com.dmdev.spring.database.entity.Company;
-import com.dmdev.spring.database.pool.repository.CrudRepository;
+import com.dmdev.spring.database.repository.CompanyRepository;
 import com.dmdev.spring.dto.CompanyReadDto;
 import com.dmdev.spring.listener.entity.EntityEvent;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.ApplicationEventPublisher;
 
 import java.util.Collections;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
 
@@ -26,27 +27,27 @@ class CompanyServiceTest {
     private static final Integer COMPANY_ID = 1;
 
     @Mock
-    private  CrudRepository<Integer, Company> companyRepository;
+    private CompanyRepository companyRepository;
     @Mock
-    private  UserService userService;
+    private UserService userService;
     @Mock
-    private  ApplicationEventPublisher eventPublisher;
+    private ApplicationEventPublisher eventPublisher;
     @InjectMocks
     private CompanyService companyService;
 
     @Test
     void findById() {
-        Mockito.doReturn(Optional.of(new Company(COMPANY_ID, null, Collections.emptyMap())))
+        doReturn(Optional.of(new Company(COMPANY_ID, null, Collections.emptyMap())))
                 .when(companyRepository).findById(COMPANY_ID);
 
-        Optional<CompanyReadDto> actualResult = companyService.findById(COMPANY_ID);
+        var actualResult = companyService.findById(COMPANY_ID);
+
         assertTrue(actualResult.isPresent());
 
-        CompanyReadDto expectedResult = new CompanyReadDto(COMPANY_ID);
+        var expectedResult = new CompanyReadDto(COMPANY_ID);
         actualResult.ifPresent(actual -> assertEquals(expectedResult, actual));
 
         verify(eventPublisher).publishEvent(any(EntityEvent.class));
         verifyNoMoreInteractions(eventPublisher, userService);
     }
-
 }
