942f3a8e987b3b3ff6b0e3ec13397019c1aa3a9f
Rest1nPeace <vitaliti081@gmail.com>
2023-07-24 17:07:12 +0300

    69.-Batch-size-_-Fetch-size

--- src/main/java/com/dmdev/spring/database/repository/FilterUserRepositoryImpl.java
+++ src/main/java/com/dmdev/spring/database/repository/FilterUserRepositoryImpl.java
@@ -12,11 +12,14 @@ import jakarta.persistence.criteria.Root;
 import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 @RequiredArgsConstructor
 public class FilterUserRepositoryImpl implements FilterUserRepository {
@@ -31,8 +34,23 @@ public class FilterUserRepositoryImpl implements FilterUserRepository {
             and role = ?
             """;
 
+    private static final String UPDATE_COMPANY_AND_ROLE = """
+            UPDATE users
+            set company_id = ?,
+            role = ?
+            where id = ?
+            """;
+
+    private static final String UPDATE_COMPANY_AND_ROLE_NAMED = """
+            UPDATE users
+            set company_id = :companyId,
+            role = :role
+            where id = :id
+            """;
+
     private final EntityManager entityManager;
     private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedJdbcTemplate;
 
     @Override
     public List<User> findAllByFilter(UserFilter filter) {
@@ -65,5 +83,25 @@ public class FilterUserRepositoryImpl implements FilterUserRepository {
                 rs.getString("lastname"),
                 rs.getDate("birth_date").toLocalDate()
         ), companyId, role.name());
+    }
+
+    @Override
+    public void updateCompanyAndRole(List<User> users) {
+        List<Object[]> args = users.stream()
+                .map(user -> new Object[]{user.getCompany().getId(), user.getRole().name(), user.getId()}).toList();
+        jdbcTemplate.batchUpdate(UPDATE_COMPANY_AND_ROLE, args);
+    }
+
+    @Override
+    public void updateCompanyAndRoleName(List<User> users) {
+        MapSqlParameterSource[] mapSqlParameterSources = users.stream()
+                .map(user -> Map.of(
+                        "companyId", user.getCompany().getId(),
+                        "role", user.getRole().name(),
+                        "id", user.getId()
+                ))
+                .map(MapSqlParameterSource::new)
+                .toArray(MapSqlParameterSource[]::new);
+        namedJdbcTemplate.batchUpdate(UPDATE_COMPANY_AND_ROLE_NAMED, mapSqlParameterSources);
     }
 }
