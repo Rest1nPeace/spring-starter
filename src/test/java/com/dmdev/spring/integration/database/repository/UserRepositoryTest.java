f8cd73253482694728361bbbb4cb5a88a618cc9f
Rest1nPeace <vitaliti081@gmail.com>
2023-07-24 13:18:42 +0300

    65.-JPA-Auditing

--- src/test/java/com/dmdev/spring/integration/database/repository/UserRepositoryTest.java
+++ src/test/java/com/dmdev/spring/integration/database/repository/UserRepositoryTest.java
@@ -26,6 +26,14 @@ class UserRepositoryTest {
     private final UserRepository userRepository;
 
     @Test
+    void checkAuditing(){
+        User user = userRepository.findById(1L).get();
+        user.setBirthDate(user.getBirthDate().plusYears(1L));
+        userRepository.flush();
+        System.out.println();
+    }
+
+    @Test
     void checkCustomImplementation(){
         UserFilter filter = new UserFilter(
                 null, "%ov%", LocalDate.now()
