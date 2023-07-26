81f5ad670b6bae074288b169a1e4fa1a004b6a42
Rest1nPeace <vitaliti081@gmail.com>
2023-07-26 13:41:40 +0300

    80.-_ModelAttribute

---
+++ src/main/java/com/dmdev/spring/dto/UserReadDto.java
@@ -0,0 +1,12 @@
+package com.dmdev.spring.dto;
+
+import lombok.Value;
+
+@Value
+public class UserReadDto {
+
+    Long id;
+
+    String username;
+
+}
--- src/main/java/com/dmdev/spring/http/controller/GreetingController.java
+++ src/main/java/com/dmdev/spring/http/controller/GreetingController.java
@@ -1,29 +1,46 @@
 package com.dmdev.spring.http.controller;
 
+import com.dmdev.spring.database.entity.Role;
+import com.dmdev.spring.dto.UserReadDto;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
+@SessionAttributes({"user"})
 public class GreetingController {
 
+    @ModelAttribute("roles")
+    public List<Role> roles(){
+        return Arrays.asList(Role.values());
+    }
+
+    @GetMapping("/hello")
+    public String hello(Model model,
+                        HttpServletRequest request,
+                        @ModelAttribute("userReadDto") UserReadDto userReadDto){
+
+        model.addAttribute("user", new UserReadDto(1L, "Iban"));
+        return "greeting/hello";
+    }
+
+    @GetMapping("/bye")
+    public String bye(@SessionAttribute("user") UserReadDto userReadDto){
+        return "greeting/bye";
+    }
+
     @GetMapping("/hello/{id}")
-    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request,
+    public ModelAndView hello2(ModelAndView modelAndView, HttpServletRequest request,
                               @RequestParam Integer age,
                               @PathVariable Integer id){
         String age1 = request.getParameter("age");
         modelAndView.setViewName("greeting/hello");
-        return modelAndView;
-    }
-
-    @GetMapping("/bye")
-    public ModelAndView bye(){
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("greeting/bye");
         return modelAndView;
     }
 }
