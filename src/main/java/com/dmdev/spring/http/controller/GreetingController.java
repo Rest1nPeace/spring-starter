3fa2321ac1c1ee10bae6eda6301a9c54a21a0ff5
Rest1nPeace <vitaliti081@gmail.com>
2023-07-26 12:36:37 +0300

    78.-Parameters_-Headers_-Cookies

--- src/main/java/com/dmdev/spring/http/controller/GreetingController.java
+++ src/main/java/com/dmdev/spring/http/controller/GreetingController.java
@@ -1,14 +1,21 @@
 package com.dmdev.spring.http.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class GreetingController {
 
-    @GetMapping("/hello")
-    public ModelAndView hello(ModelAndView modelAndView){
+    @GetMapping("/hello/{id}")
+    public ModelAndView hello(ModelAndView modelAndView, HttpServletRequest request,
+                              @RequestParam Integer age,
+                              @PathVariable Integer id){
+        String age1 = request.getParameter("age");
         modelAndView.setViewName("greeting/hello");
         return modelAndView;
     }
