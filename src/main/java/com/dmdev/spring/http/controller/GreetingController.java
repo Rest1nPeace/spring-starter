48ce6637ea5f25fd4470859f7c1b11e2815974e4
Rest1nPeace <vitaliti081@gmail.com>
2023-07-26 12:23:58 +0300

    77.-_RequestMapping

--- src/main/java/com/dmdev/spring/http/controller/GreetingController.java
+++ src/main/java/com/dmdev/spring/http/controller/GreetingController.java
@@ -1,16 +1,19 @@
 package com.dmdev.spring.http.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class GreetingController {
 
+    @GetMapping("/hello")
     public ModelAndView hello(ModelAndView modelAndView){
         modelAndView.setViewName("greeting/hello");
         return modelAndView;
     }
 
+    @GetMapping("/bye")
     public ModelAndView bye(){
         ModelAndView modelAndView = new ModelAndView();
         modelAndView.setViewName("greeting/bye");
