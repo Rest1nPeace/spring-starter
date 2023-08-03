cd24e6a2fae3a9b0459904914287e73f1ef3aae5
Rest1nPeace <vitaliti081@gmail.com>
2023-08-03 12:32:06 +0300

    93 completed

--- src/main/java/com/dmdev/spring/http/controller/UserController.java
+++ src/main/java/com/dmdev/spring/http/controller/UserController.java
@@ -7,21 +7,20 @@ import com.dmdev.spring.service.CompanyService;
 import com.dmdev.spring.service.UserService;
 import com.dmdev.spring.validation.group.CreateAction;
 import com.dmdev.spring.validation.group.UpdateAction;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.groups.Default;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@Slf4j
 @Controller
 @RequestMapping("/users")
 @RequiredArgsConstructor
@@ -29,6 +28,8 @@ public class UserController {
 
     private final UserService userService;
     private final CompanyService companyService;
+
+
 
     @GetMapping
     public String findAll(Model model, UserFilter filter) {
