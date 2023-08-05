a9da680b6406bb26a8442c37a3ed0dd782a84b44
Rest1nPeace <vitaliti081@gmail.com>
2023-08-05 18:59:59 +0300

    104 completed

--- src/main/java/com/dmdev/spring/config/SecurityConfiguration.java
+++ src/main/java/com/dmdev/spring/config/SecurityConfiguration.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -25,6 +27,11 @@ public class SecurityConfiguration {
 
 //        http.httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
