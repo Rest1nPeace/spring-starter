e0a29ac1b07f51bfad8d9188ec208c6941b05a75
Rest1nPeace <vitaliti081@gmail.com>
2023-08-07 22:20:40 +0300

    107 completed

--- src/main/java/com/dmdev/spring/http/controller/UserController.java
+++ src/main/java/com/dmdev/spring/http/controller/UserController.java
@@ -11,6 +11,7 @@ import jakarta.validation.groups.Default;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -36,6 +37,7 @@ public class UserController {
     }
 
     @GetMapping("/{id}")
+
     public String findById(@PathVariable("id") Long id, Model model) {
         return userService.findById(id)
                 .map(user -> {
